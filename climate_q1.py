035331a1631adbe77b25af8543e465c33356d7c1
Reiss Tooze <reiss.tooze@griffithuni.edu.au>
2023-09-13 10:51:27 +1000

    connect to climate.db, fetched the values from the database, and populated some Python lists with the corresponding values

--- climate_q1.py
+++ climate_q1.py
@@ -1,11 +1,23 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
+cursor.execute("SELECT year, temperature, co2 FROM climate.db")
+
 years = []
-co2 = []
+co2_d = []
 temp = []
 
+for row in cursor.fetchall():
+    year, temperature, co2 = row
+    years.append(year)
+    temp.append(temperature)
+    co2_d.append(co2)
+
+
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
+plt.plot(years, co2_d, 'b--')
 plt.title("Climate Data") 
 plt.ylabel("[CO2]") 
 plt.xlabel("Year (decade)") 
@@ -13,6 +25,7 @@ plt.xlabel("Year (decade)")
 plt.subplot(2, 1, 2)
 plt.plot(years, temp, 'r*-') 
 plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
+plt.xlabel("Year (decade)")
+plt.savefig("co2_temp_1.png")
 plt.show() 
-plt.savefig("co2_temp_1.png") 
+
